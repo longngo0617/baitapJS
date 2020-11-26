55456c5ae2c7018b7aa0622a6403b94881947f36
Long Ngo <macbook@longngo0617.local>
2020-11-26 12:46:36 +0700

    Done closure

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
-class Counter {
-     constructor(init){
-         this.count = init || 0;
-     }
-     increase(){
-        return ++this.count;
-     }
-     decrease(){
-        return --this.count;
-     }
-     reset(){
-        return this.count=0;
-     }
-};
+// class Counter {
+//      constructor(init){
+//          this.count = init || 0;
+//      }
+//      increase(){
+//         return ++this.count;
+//      }
+//      decrease(){
+//         return --this.count;
+//      }
+//      reset(){
+//         return this.count=0;
+//      }
+// };
 
-let counter = new Counter(0);
+// let counter = new Counter(0);
+
+function Counter(){
+    let count =0;
+    return {
+        increase: function(){
+            return ++count;
+        },
+        decrease: function(){
+            return --count;
+        },
+        reset: function(){
+            return count=0;
+        }
+    };
+};
 const btnIncrease = document.querySelector(".increase");
 const btnDecrease = document.querySelector(".decrease");
 const btnReset = document.querySelector(".reset");
 let number = document.getElementsByTagName('h1')[0];
-
+let count = Counter();
 btnIncrease.addEventListener('click',function(){
-    let count = counter.increase();
-    number.innerHTML=count;
+     
+    number.innerHTML=count.increase();
 })
 btnDecrease.addEventListener('click',function(){
-    let count = counter.decrease();
-    number.innerHTML=count;
+    
+    number.innerHTML=count.decrease();
 })
 btnReset.addEventListener('click',function(){
-    let count = counter.reset();
-    number.innerHTML=count;
+  
+    number.innerHTML=count.reset();
 })
 
 
