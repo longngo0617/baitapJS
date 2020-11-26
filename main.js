8a26790901bd7e2f788b31bb54c4eeef56dea652
Long Ngo <macbook@longngo0617.local>
2020-11-26 15:24:53 +0700

    Done filter

--- main.js
+++ main.js
@@ -15,37 +15,60 @@
 
 // let counter = new Counter(0);
 
-function Counter(){
-    let count =0;
-    return {
-        increase: function(){
-            return ++count;
-        },
-        decrease: function(){
-            return --count;
-        },
-        reset: function(){
-            return count=0;
-        }
-    };
-};
-const btnIncrease = document.querySelector(".increase");
-const btnDecrease = document.querySelector(".decrease");
-const btnReset = document.querySelector(".reset");
-let number = document.getElementsByTagName('h1')[0];
-let count = Counter();
-btnIncrease.addEventListener('click',function(){
+// function Counter(){
+//     let count =0;
+//     return {
+//         increase: function(){
+//             return ++count;
+//         },
+//         decrease: function(){
+//             return --count;
+//         },
+//         reset: function(){
+//             return count=0;
+//         }
+//     };
+// };
+// const btnIncrease = document.querySelector(".increase");
+// const btnDecrease = document.querySelector(".decrease");
+// const btnReset = document.querySelector(".reset");
+// let number = document.getElementsByTagName('h1')[0];
+// let count = Counter();
+// btnIncrease.addEventListener('click',function(){
      
-    number.innerHTML=count.increase();
-})
-btnDecrease.addEventListener('click',function(){
+//     number.innerHTML=count.increase();
+// })
+// btnDecrease.addEventListener('click',function(){
     
-    number.innerHTML=count.decrease();
-})
-btnReset.addEventListener('click',function(){
+//     number.innerHTML=count.decrease();
+// })
+// btnReset.addEventListener('click',function(){
   
-    number.innerHTML=count.reset();
-})
+//     number.innerHTML=count.reset();
+// })
+
+
+let arr =[1,24,5,126,21,7,79];
+
+let arr2= arr.filter(e=> e % 2==0)
+
+Object.defineProperty(Array.prototype,'filter2',{
+    value:function(callback){
+        let arr3 =[];
+        for(let i in this)
+        {
+            if(callback(this[i],i))
+            {
+                arr3.push(this[i]);
+            }
+        }
+        return arr3;
+    },
+    enumerable:false
+});
+
+let arr3 = arr.filter2(e=>e %2==0);
+console.log(arr3)
 
 
 
